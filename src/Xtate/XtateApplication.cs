7f0fc5f8af08eea81a1a3f4c92417548d0684531
Sergii Artemenko <xtadex@gmail.com>
2024-11-23 01:32:52 +0100

    Remove unused System.Collections.Concurrent imports
    Removed `using System.Collections.Concurrent;` statements from multiple source files as the code no longer relies on types from this namespace, such as `ConcurrentDictionary`.

--- src/Xtate/XtateApplication.cs
+++ src/Xtate/XtateApplication.cs
@@ -16,8 +16,6 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 using System;
-using System.Diagnostics;
-using System.IO;
 using System.Threading.Tasks;
 using Xtate.Builder;
 using Xtate.Core;
